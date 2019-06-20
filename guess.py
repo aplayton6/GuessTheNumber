9d201a965790606df94fe44e64cf64e14744d7f6
Andrew Layton <andrewlayton@Andrews-Hackintosh.local>
2019-06-20 17:38:02 -0400

    Adding guessestaken loop

--- guess.py
+++ guess.py
@@ -5,3 +5,13 @@ import random
 print('Hello! What is your name?')
 name = input()
 
+print('Well, ' + name + ' I am thinking of a number between 1 and 20.')
+secretNumber = random.randint(1,20)
+
+for guessesTaken in range(1,7):
+    print('Take a guess.)
+    guess = int(input())
+    
+    if guess < secretNumber:
+          print('Your guess is too low.')
+    
