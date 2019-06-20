fdca95b09fcbd26cee1ed14f02ed61cc950eeb01
Andrew Layton <andrewlayton@Andrews-Hackintosh.local>
2019-06-20 17:48:54 -0400

    Finished the game

--- guess.py
+++ guess.py
@@ -9,9 +9,18 @@ print('Well, ' + name + ' I am thinking of a number between 1 and 20.')
 secretNumber = random.randint(1,20)
 
 for guessesTaken in range(1,7):
-    print('Take a guess.)
+    print('Take a guess.')
     guess = int(input())
     
     if guess < secretNumber:
           print('Your guess is too low.')
-    
+    elif guess > secretNumber:
+          print('Your guess is too high.')
+    else:
+          break #This condition is for the correct guess
+
+if guess == secretNumber:
+    print('Good job, ' + name + '! You guessed the number in ' + str(guessesTaken) + ' guesses!')
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber) + '.') 
+
