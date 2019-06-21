b18b88ec8544512b6510a6b11921b69b376ee6bb
Andrew Layton <andrewlayton@Andrews-Hackintosh.local>
2019-06-20 23:14:56 -0400

    Phrase fixing

--- guess.py
+++ guess.py
@@ -5,7 +5,7 @@ import random
 print('Hello! What is your name?')
 name = input()
 
-print('Well, ' + name + ' I am thinking of a number between 1 and 20.')
+print('Well, ' + name + ' I am thinking of a number between 1 and 20. You have six guesses to get it right!')
 secretNumber = random.randint(1,20)
 
 for guessesTaken in range(1,7):
